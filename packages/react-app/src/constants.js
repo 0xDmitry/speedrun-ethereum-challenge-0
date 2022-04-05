8c96e744630624bf6dc8f7daab765c565388c511
Dmitry Filippov <filippov.d@indriver.com>
2022-04-05 13:02:34 +0300

    Done

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,8 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "e618a05e038849b78e1ee16900f01561";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "3B3XN2WH9QVZS8BZ7NH5EM4AQ6HIC9GER5";
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
